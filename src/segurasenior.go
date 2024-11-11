b80d144e0d967e105d914c33b98f4d816ea7bd23
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-11 20:26:46 +0100

    Añadido enum para Tendencia para #12
    Enum con valores de decreciente o creciente segun la tendencia de un lugar

--- src/segurasenior.go
+++ src/segurasenior.go
@@ -1,8 +1,19 @@
 package segurasenior
 
+type estadopoblacion int
 
+const (
+	Decreciente = iota
+	Creciente
+)
 
 func buscatexto(direccion string)(string){
 	//función para extraer texto del paquete net/http e io/ioutil
 	return texto
+}
+
+type Tendencia struct{
+	Nombre string
+	poblacion int
+	retransmision string
 }
