b37d1d97d41300cfcf51e287e3164c5748407ee9
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-15 17:57:01 +0100

    fixes #10 sintaxis del lenguaje

--- src/segurasenior.go
+++ src/segurasenior.go
@@ -2,34 +2,22 @@ package segurasenior
 
 type Estadopoblacion int
 
-
-
-func buscatexto(direccion string) string{
-	//función para extraer texto del paquete net/http e io/ioutil
-	return texto
-}
-
-func consultaFuentes(texto string, localidad string)(bool){
-	return strings.Contains(texto,localidad)
-}
-
 const (
 	Decreciente = iota
 	Creciente
 )
 
-func (e Estadopoblacion) String() string{
+func (e Estadopoblacion) String() string {
 	switch e {
-		case Decreciente:
-			return "decreciente"
-		case Creciente 
-			return "creciente"
+	case Decreciente:
+		return "decreciente"
+	case Creciente:
+		return "creciente"
 	}
 	return "unknown"
 }
 
-
-type Tendencia struct{
-	Nombre string
+type Tendencia struct {
+	Nombre          string
 	Estadotendencia Estadopoblacion
 }
