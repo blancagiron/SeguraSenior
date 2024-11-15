8773f4d0dc18a60ef3c2e93030789f9b3c2365c4
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-16 00:43:47 +0100

    close #11 implementación de función de conflictos
    Dado un reportaje estudiará todas sus fuentes entre sí para que no de información contradictoria en cuando al estado de la población

--- src/conflictos.go
+++ src/conflictos.go
@@ -1,6 +1,23 @@
 package segurasenior
 
-func ContrastarFuentes(reporte *Reportaje) error {
+import "fmt"
 
+func ContrastarFuentes(reporte *Reportaje) error {
+	for i := 0; i < len(reporte.Fuente); i++ {
+		fuenteog := reporte.Fuente[i]
+
+		for k := 0; k < len(reporte.Fuente); k++ {
+			otrafuente := reporte.Fuente[k]
+
+			if fuenteog.Poblacion.Nombre == otrafuente.Poblacion.Nombre && fuenteog.Poblacion.Estadotendencia == otrafuente.Poblacion.Estadotendencia {
+				continue
+			}
+
+			if fuenteog.Poblacion.ConflictoInfo(&otrafuente.Poblacion) {
+				return fmt.Errorf("informacion conflictiva entre fuentes: %s en %s ",
+					fuenteog.Direccion, otrafuente.Direccion)
+			}
+		}
+	}
 	return nil
 }
