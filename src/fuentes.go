a70314e3846b78df8d6beddbac0653db85698bad
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-15 19:42:00 +0100

    fixes #10 replanteamiento de entidad fuente

--- src/fuentes.go
+++ src/fuentes.go
@@ -1,40 +1,13 @@
 package segurasenior
 
-import (
-	"errors"
-)
-
-type Estadopoblacion int
-
-const (
-	Decreciente = iota
-	Creciente
-)
-
-func (e Estadopoblacion) String() string {
-	switch e {
-	case Decreciente:
-		return "decreciente"
-	case Creciente:
-		return "creciente"
-	}
-	return "unknown"
+type Fuente struct {
+	Nombre    string
+	Direccion string
 }
 
-type Tendencia struct {
-	Nombre          string
-	Estadotendencia Estadopoblacion
-	Direccion       string
-}
-
-func NewTendencia(nom string, estado Estadopoblacion, dir string) (*Tendencia, error) {
-	if estado != Decreciente && estado != Creciente {
-		return nil, errors.New("Estado indefinido")
-	}
-
-	return &Tendencia{
-		Nombre:          nom,
-		Estadotendencia: estado,
-		Direccion:       dir,
+func NewFuente(nom string, dir string) (*Fuente, error) {
+	return &Fuente{
+		Nombre:    nom,
+		Direccion: dir,
 	}, nil
 }
