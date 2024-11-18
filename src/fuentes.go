f7b47c8ebb7e83f7eded30fc53cae6675736a1c7
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-18 18:21:53 +0100

    fixes #10 renombramiento de las variables para incluir formato de la fuente

--- src/fuentes.go
+++ src/fuentes.go
@@ -1,14 +1,14 @@
 package segurasenior
 
 type Fuente struct {
-	Origen    string //Nombre reducido de la página o especificación de la extensión para entendimiento del origen de la fuente
+	Formato   string //Nombre reducido de la página o especificación de la extensión para entendimiento del origen de la fuente
 	Direccion string
 	Poblacion Tendencia
 }
 
-func NewFuente(nom string, dir string, ten Tendencia) (*Fuente, error) {
+func NewFuente(ext string, dir string, ten Tendencia) (*Fuente, error) {
 	return &Fuente{
-		Origen:    nom,
+		Formato:   ext,
 		Direccion: dir,
 		Poblacion: ten,
 	}, nil
