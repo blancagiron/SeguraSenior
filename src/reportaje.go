28fa240a66e76fddbc0f8e856cf58ed863228b3e
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-18 20:18:01 +0100

    fixes #9 corrección de sintaxis

--- src/reportaje.go
+++ src/reportaje.go
@@ -1,16 +1,17 @@
 package segurasenior
 
 type Reportaje struct {
-	nombre        string
-	poblacion     int
-	retransmision string
+	Nombre        string
+	Poblacion     int
+	Retransmision string
 	Fuente        []Fuente
 }
 
 func AniadirRepo(nom string, pobla int, medio string, orig []Fuente) *Reportaje {
 	return &Reportaje{
-		nombre:        nom,
-		poblacion:     pobla,
-		retransmision: medio,
+		Nombre:        nom,
+		Poblacion:     pobla,
+		Retransmision: medio,
+		Fuente:        orig,
 	}
 }
