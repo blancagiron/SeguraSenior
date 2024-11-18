4d54775deb2725adc03ce86ab33cca271be17a76
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-18 19:33:06 +0100

    fixes #12 añadidos atributos para mejor análisis de la población
    añadidos parámetros de población actual y tasa de natalidad, envejecimiento y mortalidad estas tres últimas requieren de valores entre 0 y 1000

--- src/tendencia.go
+++ src/tendencia.go
@@ -24,16 +24,28 @@ func (e Estadopoblacion) String() string {
 type Tendencia struct {
 	Nombre          string
 	Estadotendencia Estadopoblacion
+	PoblacionTotal  int
+	Tasanat         int //medido en tantos por mil
+	Tasaenv         int //medido en tantos por mil
+	Tasamort        int //medido en tantos por mil
 }
 
-func NewTendencia(nom string, estado Estadopoblacion) (*Tendencia, error) {
+func NewTendencia(nom string, estado Estadopoblacion, poblacion int, tasan int, tasae int, tasam int) (*Tendencia, error) {
 	if estado != Decreciente && estado != Creciente {
 		return nil, errors.New("estado indefinido")
+	}
+
+	if (tasan < 0) || (tasan > 1000) || (tasae < 0) || (tasae > 1000) || (tasam < 0) || (tasam > 1000) {
+		return nil, errors.New("valor de tasas erróneo, debe estar comprendido entre 0 y 1000")
 	}
 
 	return &Tendencia{
 		Nombre:          nom,
 		Estadotendencia: estado,
+		PoblacionTotal:  poblacion,
+		Tasanat:         tasan,
+		Tasaenv:         tasae,
+		Tasamort:        tasam,
 	}, nil
 }
 
