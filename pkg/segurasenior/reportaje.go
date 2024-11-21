8ad186e74622184ac8b96530c3e1ab9458515f66
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-21 10:32:04 +0100

    #9 replantea variables de reportaje
    A la hora de trabajar con un reportaje no debería influir el medio al que va destinado, también se ha eliminado el número de la población por estar ya declarado en el tipo de dato población y separadas fuentes de tendencias para contener por un lado la información y origen de esta y por otro la conclusión de la información sobre una población concreta

--- pkg/segurasenior/reportaje.go
+++ pkg/segurasenior/reportaje.go
@@ -1,17 +1,15 @@
 package segurasenior
 
 type Reportaje struct {
-	Nombre        string
-	Poblacion     int
-	Retransmision string
-	Fuentes       []Fuente
+	Nombre              string
+	Tendencia_reportaje Tendencia
+	Fuentes             []Fuente
 }
 
-func AniadirRepo(nom string, pobla int, medio string, orig []Fuente) *Reportaje {
+func AniadirRepo(nom string, ten Tendencia, orig []Fuente) *Reportaje {
 	return &Reportaje{
-		Nombre:        nom,
-		Poblacion:     pobla,
-		Retransmision: medio,
-		Fuentes:       orig,
+		Nombre:              nom,
+		Tendencia_reportaje: ten,
+		Fuentes:             orig,
 	}
 }
