606a1afa5bb4b3de1f6320304a4e7842fa9925ae
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-21 10:04:11 +0100

    #10 replanteamiento de las variables de fuentes
    Una fuente debería servir solo para contener la información de la que se extrae luego en un reportaje el veredicto del estado de una población

--- pkg/segurasenior/fuentes.go
+++ pkg/segurasenior/fuentes.go
@@ -3,13 +3,11 @@ package segurasenior
 type Fuente struct {
 	Formato   string
 	Direccion string
-	Poblacion Tendencia
 }
 
-func NewFuente(ext string, dir string, ten Tendencia) (*Fuente, error) {
+func NewFuente(ext string, dir string) (*Fuente, error) {
 	return &Fuente{
 		Formato:   ext,
 		Direccion: dir,
-		Poblacion: ten,
 	}, nil
 }
