a73707bc41cf79a79f30014d3e82ad9d6ad8007a
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-21 21:55:25 +0100

    #12 repaso de sintaxis
    Siguiendo las convenciones de golang

--- pkg/segurasenior/tendencia.go
+++ pkg/segurasenior/tendencia.go
@@ -7,34 +7,34 @@ import (
 type Estadopoblacion int
 
 const (
-	Decreciente = iota
-	Creciente
+	decreciente = iota
+	creciente
 )
 
 func (e Estadopoblacion) String() string {
 	switch e {
-	case Decreciente:
+	case decreciente:
 		return "decreciente"
-	case Creciente:
+	case creciente:
 		return "creciente"
 	}
 	return "unknown"
 }
 
 type Tendencia struct {
-	Nombre_pueblo_tendencia string
-	Estadotendencia         Estadopoblacion
-	Poblacion               Poblacion
+	nombre_pueblo_tendencia string
+	estadotendencia         Estadopoblacion
+	poblacion               Poblacion
 }
 
 func NewTendencia(nom string, estado Estadopoblacion, pobl Poblacion) (*Tendencia, error) {
-	if estado != Decreciente && estado != Creciente {
+	if estado != decreciente && estado != creciente {
 		return nil, errors.New("estado indefinido")
 	}
 
 	return &Tendencia{
-		Nombre_pueblo_tendencia: nom,
-		Estadotendencia:         estado,
-		Poblacion:               pobl,
+		nombre_pueblo_tendencia: nom,
+		estadotendencia:         estado,
+		poblacion:               pobl,
 	}, nil
 }
