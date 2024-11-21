c4d97c7a429871e81e8820dc071ec7c673cf9ce7
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-21 22:05:57 +0100

    #10 repaso de sintaxis
    Siguiendo las convenciones de golang, variables en minúscula

--- pkg/segurasenior/fuentes.go
+++ pkg/segurasenior/fuentes.go
@@ -5,34 +5,34 @@ import "errors"
 type Formato int
 
 const (
-	Html = iota
-	Pdf
-	Excel
+	html = iota
+	pdf
+	excel
 )
 
 func (f Formato) String() string {
 	switch f {
-	case Html:
+	case html:
 		return "html"
-	case Pdf:
+	case pdf:
 		return "pdf"
-	case Excel:
+	case excel:
 		return "excel"
 	}
 	return "unknown"
 }
 
 type Fuente struct {
-	Formato_fuente Formato
-	Direccion      string
+	formato_fuente Formato
+	direccion      string
 }
 
 func NewFuente(ext Formato, dir string) (*Fuente, error) {
-	if ext != Html && ext != Pdf && ext != Excel {
+	if ext != html && ext != pdf && ext != excel {
 		return nil, errors.New("Formato indefinido")
 	}
 	return &Fuente{
-		Formato_fuente: ext,
-		Direccion:      dir,
+		formato_fuente: ext,
+		direccion:      dir,
 	}, nil
 }
