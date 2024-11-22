0b81875691a69a74543744c7041018c3f9930e56
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-22 13:06:58 +0100

    #12 cambio nombre variable del enum
    Según el documento de desarrollo de go, empiezan por mayúscula la declaración del int de posición

--- pkg/segurasenior/tendencia.go
+++ pkg/segurasenior/tendencia.go
@@ -4,14 +4,14 @@ import (
 	"errors"
 )
 
-type estado int
+type Estado int
 
 const (
 	decreciente = iota
 	creciente
 )
 
-func (e estado) String() string {
+func (e Estado) String() string {
 	switch e {
 	case decreciente:
 		return "decreciente"
@@ -23,11 +23,11 @@ func (e estado) String() string {
 
 type Tendencia struct {
 	nombre_pueblo_tendencia string
-	estadotendencia         estado
+	estadotendencia         Estado
 	poblacion               Poblacion
 }
 
-func NewTendencia(nom string, estado estado, pobl Poblacion) (*Tendencia, error) {
+func NewTendencia(nom string, estado Estado, pobl Poblacion) (*Tendencia, error) {
 	if estado != decreciente && estado != creciente {
 		return nil, errors.New("estado indefinido")
 	}
