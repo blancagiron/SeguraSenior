9b2355be634c4b6b3ef9a1bceaa8ee329bb8d919
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:23:22 +0100

    #10 añadido a fuentes nombre de la población
    Para especificar en un tipo de entidad fuente de qué población habla específicamente

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -11,18 +11,23 @@ const (
 )
 
 type Fuente struct {
-	nombre_fuente  string
-	formato_fuente FormatoFuente
-	direccion      string
+	nombre_fuente           string
+	nombre_poblacion_fuente string
+	formato_fuente          FormatoFuente
+	direccion               string
 }
 
-func NewFuente(nom string, ext FormatoFuente, dir string) (*Fuente, error) {
+func NewFuente(nom string, nompoblacion string, ext FormatoFuente, dir string) (*Fuente, error) {
+	if nompoblacion == "" {
+		return nil, errors.New("una fuente debe hablar de alguna población")
+	}
 	if ext != html && ext != pdf && ext != excel {
 		return nil, errors.New("formato indefinido")
 	}
 	return &Fuente{
-		nombre_fuente:  nom,
-		formato_fuente: ext,
-		direccion:      dir,
+		nombre_fuente:           nom,
+		nombre_poblacion_fuente: nompoblacion,
+		formato_fuente:          ext,
+		direccion:               dir,
 	}, nil
 }
