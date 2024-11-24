b9cb842c3c64c06588c916143bf5976be5cf69db
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:33:48 +0100

    #9 añadido dato tipo fuentes
    Un reportaje siempre requiere de mostar sus fuentes y pueden provenir de múltiples sitios

--- pkg/segurasenior/reportaje.go
+++ pkg/segurasenior/reportaje.go
@@ -4,12 +4,14 @@ type Reportaje struct {
 	nombre          string
 	tendencias      []Tendencia
 	datos_poblacion []Poblacion
+	fuentes         []Fuente
 }
 
-func NewRepo(nom string, ten []Tendencia, datospob []Poblacion) (*Reportaje, error) {
+func NewRepo(nom string, ten []Tendencia, datospob []Poblacion, origen []Fuente) (*Reportaje, error) {
 	return &Reportaje{
 		nombre:          nom,
 		tendencias:      ten,
 		datos_poblacion: datospob,
+		fuentes:         origen,
 	}, nil
 }
