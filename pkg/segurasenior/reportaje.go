65b7229bd350b115b19dff62080515a3fbaaff4b
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:31:45 +0100

    #9 añadido tipo de dato array para poblaciones
    Un reportaje consta de distinta información sobre una o varias poblaciones distintas

--- pkg/segurasenior/reportaje.go
+++ pkg/segurasenior/reportaje.go
@@ -1,13 +1,15 @@
 package segurasenior
 
 type Reportaje struct {
-	nombre     string
-	tendencias []Tendencia
+	nombre          string
+	tendencias      []Tendencia
+	datos_poblacion []Poblacion
 }
 
-func NewRepo(nom string, ten []Tendencia) (*Reportaje, error) {
+func NewRepo(nom string, ten []Tendencia, datospob []Poblacion) (*Reportaje, error) {
 	return &Reportaje{
-		nombre:     nom,
-		tendencias: ten,
+		nombre:          nom,
+		tendencias:      ten,
+		datos_poblacion: datospob,
 	}, nil
 }
