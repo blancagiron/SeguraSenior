5fcf8fd5c906b330a3699191124dff9c93be2cdd
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:27:58 +0100

    #9 tipo de dato tendencia cambiado a array
    necesitamos varias tendencias para deducir el resultado de una población reflejado en el reportaje

--- pkg/segurasenior/reportaje.go
+++ pkg/segurasenior/reportaje.go
@@ -1,13 +1,13 @@
 package segurasenior
 
 type Reportaje struct {
-	nombre              string
-	tendencia_reportaje Tendencia
+	nombre     string
+	tendencias []Tendencia
 }
 
-func NewRepo(nom string, ten Tendencia) (*Reportaje, error) {
+func NewRepo(nom string, ten []Tendencia) (*Reportaje, error) {
 	return &Reportaje{
-		nombre:              nom,
-		tendencia_reportaje: ten,
+		nombre:     nom,
+		tendencias: ten,
 	}, nil
 }
