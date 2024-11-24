4a4545eb093699cf4a1b225027e031264dba4f16
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:20:22 +0100

    #10 añadido NewFuente

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -1,5 +1,7 @@
 package segurasenior
 
+import "errors"
+
 type FormatoFuente string
 
 const (
@@ -12,4 +14,15 @@ type Fuente struct {
 	nombre_fuente  string
 	formato_fuente FormatoFuente
 	direccion      string
+}
+
+func NewFuente(nom string, ext FormatoFuente, dir string) (*Fuente, error) {
+	if ext != html && ext != pdf && ext != excel {
+		return nil, errors.New("formato indefinido")
+	}
+	return &Fuente{
+		nombre_fuente:  nom,
+		formato_fuente: ext,
+		direccion:      dir,
+	}, nil
 }
