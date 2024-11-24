237882de84e9bc8b7dd9d62b6212ba553fd4032f
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-24 18:19:19 +0100

    #10 añadido tipo de dato para almacenar formato de la fuente
    FormatoFuente nos deja elegir entre los tipos buscados por el cliente pdf, excel o archivo html

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -1,6 +1,15 @@
 package segurasenior
 
+type FormatoFuente string
+
+const (
+	html  FormatoFuente = "html"
+	pdf   FormatoFuente = "pdf"
+	excel FormatoFuente = "excel"
+)
+
 type Fuente struct {
-	Nombre    string
-	direccion string
+	nombre_fuente  string
+	formato_fuente FormatoFuente
+	direccion      string
 }
