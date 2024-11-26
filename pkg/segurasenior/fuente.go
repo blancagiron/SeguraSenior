95904679080f89177c8cac974243f3111f42ca52
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-26 14:45:51 +0100

    fixes #10 repaso de errores
    No comprobamos el valor de variables que ya no trabajamos

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -5,21 +5,16 @@ import "errors"
 type Fuente struct {
 	Nombre_fuente           string
 	Nombre_poblacion_fuente string
-	Formato_fuente          FormatoFuente
 	Direccion               string
 }
 
-func NewFuente(nom string, nompoblacion string, ext FormatoFuente, dir string) (*Fuente, error) {
+func NewFuente(nom string, nompoblacion string, dir string) (*Fuente, error) {
 	if nompoblacion == "" {
 		return nil, errors.New("una fuente debe hablar de alguna población")
-	}
-	if ext != HTML && ext != PDF && ext != EXCEL {
-		return nil, errors.New("formato indefinido")
 	}
 	return &Fuente{
 		Nombre_fuente:           nom,
 		Nombre_poblacion_fuente: nompoblacion,
-		Formato_fuente:          ext,
 		Direccion:               dir,
 	}, nil
 }
