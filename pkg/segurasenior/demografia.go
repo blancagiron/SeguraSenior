d7a83ebea40bc5956212940b1421251610b63396
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-27 12:44:33 +0100

    #9 añadida función de inicializar objeto demografía

--- pkg/segurasenior/demografia.go
+++ pkg/segurasenior/demografia.go
@@ -1,7 +1,20 @@
 package segurasenior
 
+import "errors"
+
 type Registro_demografico struct {
 	Estadisticas_poblacion []Datos_poblacion
 	Tendencias_poblacion   Tendencia
 	Fuentes                []Fuente
+}
+
+func NewRegistro(poblaciones []Datos_poblacion, fuentes []Fuente, tendencia Tendencia) (*Registro_demografico, error) {
+	if len(poblaciones) == 0 {
+		return nil, errors.New("el registro debe incluir algún dato sobre la población")
+	}
+	return &Registro_demografico{
+		Estadisticas_poblacion: poblaciones,
+		Tendencias_poblacion:   tendencia,
+		Fuentes:                fuentes,
+	}, nil
 }
