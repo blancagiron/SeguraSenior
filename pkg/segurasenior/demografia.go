5d4abf6a5b149159dc002b48773c20b22f23cfc7
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-27 12:41:59 +0100

    #9 repaso nombre de las variables

--- pkg/segurasenior/demografia.go
+++ pkg/segurasenior/demografia.go
@@ -3,5 +3,5 @@ package segurasenior
 type Registro_demografico struct {
 	Estadisticas_poblacion []Datos_poblacion
 	Tendencias_poblacion   Tendencia
-	fuentes                []Fuente
+	Fuentes                []Fuente
 }
