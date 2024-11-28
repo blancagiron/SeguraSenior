efc08c063087b6cc5c39c52a75694f1cd44b537d
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-28 08:46:20 +0100

    #10 omisión del atributo nombre de fuente
    No es especialmente relevante para el desarrollo o trabajo con la entidad de fuentes

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -3,17 +3,15 @@ package segurasenior
 import "errors"
 
 type Fuente struct {
-	Nombre_fuente           string
 	Nombre_poblacion_fuente string
 	Direccion               string
 }
 
-func NewFuente(nom string, nompoblacion string, dir string) (*Fuente, error) {
+func NewFuente(nompoblacion string, dir string) (*Fuente, error) {
 	if nompoblacion == "" {
 		return nil, errors.New("una fuente debe hablar de alguna población")
 	}
 	return &Fuente{
-		Nombre_fuente:           nom,
 		Nombre_poblacion_fuente: nompoblacion,
 		Direccion:               dir,
 	}, nil
