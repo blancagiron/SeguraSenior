2a860dc7cc3c0b0055bbc7d58b82c70c24a8da58
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-28 10:29:02 +0100

    #10 eliminada validación del tipo de dato string de nombre de población

--- pkg/segurasenior/fuente.go
+++ pkg/segurasenior/fuente.go
@@ -1,15 +1,11 @@
 package segurasenior
 
-import "errors"
-
 type Fuente struct {
 	Direccion string
 }
 
 func NewFuente(dir string) (*Fuente, error) {
-	if nompoblacion == "" {
-		return nil, errors.New("una fuente debe hablar de alguna población")
-	}
+
 	return &Fuente{
 		Direccion: dir,
 	}, nil
