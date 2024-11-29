6978ceda1fba5518c066c657798571c638562884
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-29 13:16:55 +0100

    #13 cambio de nombre de variable tasa de natalidad

--- pkg/segurasenior/poblacion.go
+++ pkg/segurasenior/poblacion.go
@@ -13,12 +13,13 @@ type Datos_poblacion struct {
 	Tasa_mortalidad     float64
 }
 
-func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, poblacion_entre_18_y_64 uint32, poblacion_mayor_64 uint32, tasan float64, tasae float64, tasam float64) (*Datos_poblacion, error) {
+func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, poblacion_entre_18_y_64 uint32, poblacion_mayor_64 uint32,
+	tasa_natalidad float64, tasae float64, tasam float64) (*Datos_poblacion, error) {
 	if nom == "" {
 		return nil, errors.New("nombre no puede estar vacío")
 	}
 
-	if (tasan < 0.0) || (tasan > 1000.0) {
+	if (tasa_natalidad < 0.0) || (tasa_natalidad > 1000.0) {
 		return nil, errors.New("valor de tasa de natalidada erróneo, debe estar comprendido entre 0 y 1000")
 	}
 	if (tasae < 0.0) || (tasae > 1000.0) {
@@ -33,7 +34,7 @@ func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, pobla
 		Poblacion_joven:     poblacion_menor_18,
 		Poblacion_adulta:    poblacion_entre_18_y_64,
 		Poblacion_anciana:   poblacion_mayor_64,
-		Tasa_natalidad:      tasan,
+		Tasa_natalidad:      tasa_natalidad,
 		Tasa_envejecimiento: tasae,
 		Tasa_mortalidad:     tasam,
 	}, nil
