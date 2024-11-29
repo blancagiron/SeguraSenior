a0977deb35ee56fe088e7fb8bb2ca9dec7f914e5
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-29 13:51:13 +0100

    #13 cambio nombre variable tasa natalidad
    Para especificar que es un valor fraccionario sobre 1000

--- pkg/segurasenior/poblacion.go
+++ pkg/segurasenior/poblacion.go
@@ -14,12 +14,12 @@ type Datos_poblacion struct {
 }
 
 func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, poblacion_entre_18_y_64 uint32, poblacion_mayor_64 uint32,
-	tasa_natalidad float64, tasa_envejecimiento float64, tasa_mortalidad float64) (*Datos_poblacion, error) {
+	tasa_natalidad_sobre_1000 float64, tasa_envejecimiento float64, tasa_mortalidad float64) (*Datos_poblacion, error) {
 	if nom == "" {
 		return nil, errors.New("nombre no puede estar vacío")
 	}
 
-	if (tasa_natalidad < 0.0) || (tasa_natalidad > 1000.0) {
+	if (tasa_natalidad_sobre_1000 < 0.0) || (tasa_natalidad_sobre_1000 > 1000.0) {
 		return nil, errors.New("valor de tasa de natalidada erróneo, debe estar comprendido entre 0 y 1000")
 	}
 	if (tasa_envejecimiento < 0.0) || (tasa_envejecimiento > 1000.0) {
@@ -34,7 +34,7 @@ func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, pobla
 		Poblacion_joven:     poblacion_menor_18,
 		Poblacion_adulta:    poblacion_entre_18_y_64,
 		Poblacion_anciana:   poblacion_mayor_64,
-		Tasa_natalidad:      tasa_natalidad,
+		Tasa_natalidad:      tasa_natalidad_sobre_1000,
 		Tasa_envejecimiento: tasa_envejecimiento,
 		Tasa_mortalidad:     tasa_mortalidad,
 	}, nil
