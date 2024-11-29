83ff9b9bb60dfd5cba0b3149f513a9b64ad539c4
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-29 13:49:53 +0100

    #9 eliminación de variable de fuentes
    No es relevante para el modelo de datos o manejo de información sobre la población

--- pkg/segurasenior/demografia.go
+++ pkg/segurasenior/demografia.go
@@ -10,14 +10,12 @@ const (
 type Registro_demografico struct {
 	Estadisticas_poblacion Datos_poblacion
 	Tendencias_poblacion   Estado
-	Direcciones_fuentes    []string
 }
 
-func NewRegistro(poblacion Datos_poblacion, fuentes []string, tendencia Estado) (*Registro_demografico, error) {
+func NewRegistro(poblacion Datos_poblacion, tendencia Estado) (*Registro_demografico, error) {
 	return &Registro_demografico{
 		Estadisticas_poblacion: poblacion,
 		Tendencias_poblacion:   tendencia,
-		Direcciones_fuentes:    fuentes,
 	}, nil
 }
 
