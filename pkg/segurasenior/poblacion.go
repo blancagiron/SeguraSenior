63829dfdc50c4f2148de6c073ceaaaeb1839c6a3
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-11-29 14:24:51 +0100

    #13 cambiada sintaxis de snake case a camel case

--- pkg/segurasenior/poblacion.go
+++ pkg/segurasenior/poblacion.go
@@ -3,39 +3,39 @@ package segurasenior
 import "errors"
 
 type Datos_poblacion struct {
-	Nombre_pueblo       string
-	Poblacion_total     uint32
-	Poblacion_joven     uint32
-	Poblacion_adulta    uint32
-	Poblacion_anciana   uint32
-	Tasa_natalidad      float64
-	Tasa_envejecimiento float64
-	Tasa_mortalidad     float64
+	NombrePueblo       string
+	PoblacionTotal     uint32
+	PoblacionJoven     uint32
+	PoblacionAdulta    uint32
+	PoblacionAnciana   uint32
+	TasaNatalidad      float64
+	TasaEnvejecimiento float64
+	TasaMortalidad     float64
 }
 
-func NewPoblacion(nom string, poblacion uint32, poblacion_menor_18 uint32, poblacion_entre_18_y_64 uint32, poblacion_mayor_64 uint32,
-	tasa_natalidad_sobre_1000 float64, tasa_envejecimiento_sobre_1000 float64, tasa_mortalidad_sobre_1000 float64) (*Datos_poblacion, error) {
+func NewPoblacion(nom string, poblacion uint32, poblacionMenor18 uint32, poblacionEntre18Y64 uint32, poblacionMayor64 uint32,
+	tasaNatalidadSobre1000 float64, tasaEnvejecimientoSobre1000 float64, tasaMortalidadSobre1000 float64) (*Datos_poblacion, error) {
 	if nom == "" {
 		return nil, errors.New("nombre no puede estar vacío")
 	}
 
-	if (tasa_natalidad_sobre_1000 < 0.0) || (tasa_natalidad_sobre_1000 > 1000.0) {
+	if (tasaNatalidadSobre1000 < 0.0) || (tasaNatalidadSobre1000 > 1000.0) {
 		return nil, errors.New("valor de tasa de natalidada erróneo, debe estar comprendido entre 0 y 1000")
 	}
-	if (tasa_envejecimiento_sobre_1000 < 0.0) || (tasa_envejecimiento_sobre_1000 > 1000.0) {
+	if (tasaEnvejecimientoSobre1000 < 0.0) || (tasaEnvejecimientoSobre1000 > 1000.0) {
 		return nil, errors.New("valor de tasa de envejecimiento erróneo, debe estar comprendido entre 0 y 1000")
 	}
-	if (tasa_mortalidad_sobre_1000 < 0.0) || (tasa_mortalidad_sobre_1000 > 1000.0) {
+	if (tasaMortalidadSobre1000 < 0.0) || (tasaMortalidadSobre1000 > 1000.0) {
 		return nil, errors.New("valor de tasa de mortalidad erróneo, debe estar comprendido entre 0 y 1000")
 	}
 	return &Datos_poblacion{
-		Nombre_pueblo:       nom,
-		Poblacion_total:     poblacion,
-		Poblacion_joven:     poblacion_menor_18,
-		Poblacion_adulta:    poblacion_entre_18_y_64,
-		Poblacion_anciana:   poblacion_mayor_64,
-		Tasa_natalidad:      tasa_natalidad_sobre_1000,
-		Tasa_envejecimiento: tasa_envejecimiento_sobre_1000,
-		Tasa_mortalidad:     tasa_mortalidad_sobre_1000,
+		NombrePueblo:       nom,
+		PoblacionTotal:     poblacion,
+		PoblacionJoven:     poblacionMenor18,
+		PoblacionAdulta:    poblacionEntre18Y64,
+		PoblacionAnciana:   poblacionMayor64,
+		TasaNatalidad:      tasaNatalidadSobre1000,
+		TasaEnvejecimiento: tasaEnvejecimientoSobre1000,
+		TasaMortalidad:     tasaMortalidadSobre1000,
 	}, nil
 }
