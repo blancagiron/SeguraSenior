df1cc382917115a0abd4891c097e815d498f12f1
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-12-02 17:13:09 +0100

    #9 repaso de errores tipográficos

---
+++ pkg/segurasenior/registrodemografico.go
@@ -0,0 +1,22 @@
+package segurasenior
+
+type Estado string
+
+const (
+	Decreciente Estado = "decreciente"
+	Creciente   Estado = "creciente"
+)
+
+type RegistroDemografico struct {
+	EstadisticasPoblacion DatosPoblacion
+	TendenciasPoblacion   Estado
+}
+
+func NewRegistroDemografico(poblacion DatosPoblacion, tendencia Estado) (*RegistroDemografico, error) {
+	return &RegistroDemografico{
+		EstadisticasPoblacion: poblacion,
+		TendenciasPoblacion:   tendencia,
+	}, nil
+}
+
+type Registros_demografia map[string][]RegistroDemografico
