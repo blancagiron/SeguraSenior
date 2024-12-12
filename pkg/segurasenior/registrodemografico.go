60337fe321a727d36327f41ff66952366614744a
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-12-12 12:28:58 +0100

    #9 identificador de Datospoblacion movido a su correspondiente archivo de entidad

--- pkg/segurasenior/registrodemografico.go
+++ pkg/segurasenior/registrodemografico.go
@@ -12,19 +12,8 @@ const (
 	Creciente   EstadoPoblacion = "creciente"
 )
 
-type FechaObtencionDeDatos struct {
-	Dia  uint16
-	Mes  time.Month
-	Anio uint16
-}
-
 func daysIn(mes time.Month, anio uint16) int {
 	return time.Date(int(anio), mes+1, 0, 0, 0, 0, 0, time.UTC).Day()
-}
-
-type IdentificadorDatos struct {
-	NombrePoblacion string
-	FechaDeDatos    FechaObtencionDeDatos
 }
 
 type RegistroDemografico struct {
