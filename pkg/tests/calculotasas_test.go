dcd679fcb7ed65be1b8661b9315a5fa3db287b80
blancagiron <blancagiron@correo.ugr.es>
2025-01-16 23:34:01 +0100

    Closes #35 - Se modifica la estructura de los tests, implementando una tabla de casos de prueba - Así evitamos la repetición de código - Podemos comprobar el comportamiento de la función ante diferentes casos - En este caso no se han producido ni nacimientos ni defunciones por lo que el resultado debe ser 0.

--- pkg/tests/calculotasas_test.go
+++ pkg/tests/calculotasas_test.go
@@ -8,17 +8,44 @@ import (
 
 func TestCalcularTasas(t *testing.T) {
 
-	datos := segurasenior.DatosPoblacion{
-		PoblacionTotal: 350,
-		Nacimientos:    20,
-		Defunciones:    10,
+	tests := []struct {
+		name                      string
+		datosEntrada              segurasenior.DatosPoblacion
+		expectedTasaNatalidad     float64
+		expectedTasaMortalidad    float64
+	}{
+		{
+			name: "Caso 1: Datos normales",
+			datosEntrada: segurasenior.DatosPoblacion{
+				PoblacionTotal: 350,
+				Nacimientos:    20,
+				Defunciones:    10,
+			},
+			expectedTasaNatalidad: 57.14,
+			expectedTasaMortalidad: 28.57,
+		},
+		{
+			name: "Caso 2: Población de 1000, sin nacimientos ni defunciones",
+			datosEntrada: segurasenior.DatosPoblacion{
+				PoblacionTotal: 1000,
+				Nacimientos:    0,
+				Defunciones:    0,
+			},
+			expectedTasaNatalidad: 0.00,
+			expectedTasaMortalidad: 0.00,
+		},
+		
 	}
 
-	expectedTasaNatalidad := 57.14
-	expectedTasaMortalidad := 28.57
-
-	datos.CalcularTasas()
-
-	assert.Equal(t, expectedTasaNatalidad, datos.TasaNatalidadSobre1000, "Tasa de natalidad incorrecta")
-	assert.Equal(t, expectedTasaMortalidad, datos.TasaMortalidadSobre1000, "Tasa de mortalidad incorrecta")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			
+			datos := tt.datosEntrada
+			
+			datos.CalcularTasas()
+	
+			assert.InDelta(t, tt.expectedTasaNatalidad, datos.TasaNatalidadSobre1000, 0.01, "Tasa de natalidad incorrecta")
+			assert.InDelta(t, tt.expectedTasaMortalidad, datos.TasaMortalidadSobre1000, 0.01, "Tasa de mortalidad incorrecta")
+		})
+	}
 }
