318f0e5df9a0be27b3c53b8edd0dc113ebdb4989
blancagiron <blancagiron@correo.ugr.es>
2025-01-17 01:06:10 +0100

    Closes #40: Se añade un nuevo test para probar el comportamiento de la función si intenta extraer datos de un archivo que no existe. Se cambia la estructura de los tests usando una tabla.
    - no_existe.json no existe en la estructura
    - assert.Error(t, err) comprueba que la función devuelve el error, si no lo hace, el test falla.

--- pkg/tests/extracciondatos_test.go
+++ pkg/tests/extracciondatos_test.go
@@ -3,57 +3,67 @@ package segurasenior
 import (
 	"SeguraSenior/pkg/segurasenior"
 	"github.com/stretchr/testify/assert"
-	"testing"
 	"math"
+	"testing"
 )
 
 func TestLecturaCorrectaDatosDesdeJSON(t *testing.T) {
-	
-	nombreArchivo := "../testdata/data.json"
-	nombrePoblacion := "Orcera"
 
-	
-	const (
-		poblacionTotal          uint32  = 1735
-		hombres                 uint32  = 868
-		mujeres                 uint32  = 867
-		edadMedia               float32  = 42.2
-		porcentajeMenor20       float64 = 15.6
-		porcentajeMayor65       float64 = 22.1
-		nacimientos             uint32  = 6
-		defunciones             uint32  = 29
-	)
+	t.Run("Lectura Correcta - Caso Válido", func(t *testing.T) {
+		nombreArchivo := "../testdata/data.json"
+		nombrePoblacion := "Orcera"
 
-	tasaNatalidadSobre1000 := math.Round((float64(nacimientos)/float64(poblacionTotal))*1000*100) / 100
-	tasaMortalidadSobre1000 := math.Round((float64(defunciones)/float64(poblacionTotal))*1000*100) / 100
+		const (
+			poblacionTotal    uint32  = 1735
+			hombres           uint32  = 868
+			mujeres           uint32  = 867
+			edadMedia         float32 = 42.2
+			porcentajeMenor20 float64 = 15.6
+			porcentajeMayor65 float64 = 22.1
+			nacimientos       uint32  = 6
+			defunciones       uint32  = 29
+		)
 
-	expectedIdentificador := segurasenior.IdentificadorDatos{
-		NombrePoblacion: nombrePoblacion,
-		FechaDeDatos: segurasenior.FechaObtencionDeDatos{
-			Dia:  1,
-			Mes:  1,
-			Anio: 2023,
-		},
-	}
+		tasaNatalidadSobre1000 := math.Round((float64(nacimientos)/float64(poblacionTotal))*1000*100) / 100
+		tasaMortalidadSobre1000 := math.Round((float64(defunciones)/float64(poblacionTotal))*1000*100) / 100
 
-	expectedDatos := segurasenior.DatosPoblacion{
-		PoblacionTotal:          poblacionTotal,
-		Hombres:                 hombres,
-		Mujeres:                 mujeres,
-		EdadMedia:               edadMedia,
-		PorcentajeMenora20:      porcentajeMenor20,
-		PorcentajeMayora65:      porcentajeMayor65,
-		Nacimientos:             nacimientos,
-		Defunciones:             defunciones,
-		TasaNatalidadSobre1000:  tasaNatalidadSobre1000,
-		TasaMortalidadSobre1000: tasaMortalidadSobre1000,
-	}
+		expectedIdentificador := segurasenior.IdentificadorDatos{
+			NombrePoblacion: nombrePoblacion,
+			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
+				Dia:  1,
+				Mes:  1,
+				Anio: 2023,
+			},
+		}
 
-	
-	identificador, datosExtraidos, err := segurasenior.LeerDatosDesdeJSON(nombreArchivo, nombrePoblacion)
+		expectedDatos := segurasenior.DatosPoblacion{
+			PoblacionTotal:          poblacionTotal,
+			Hombres:                 hombres,
+			Mujeres:                 mujeres,
+			EdadMedia:               edadMedia,
+			PorcentajeMenora20:      porcentajeMenor20,
+			PorcentajeMayora65:      porcentajeMayor65,
+			Nacimientos:             nacimientos,
+			Defunciones:             defunciones,
+			TasaNatalidadSobre1000:  tasaNatalidadSobre1000,
+			TasaMortalidadSobre1000: tasaMortalidadSobre1000,
+		}
 
-	assert.NoError(t, err)
-	assert.Equal(t, expectedIdentificador, identificador)
-	assert.Equal(t, expectedDatos, datosExtraidos)
+		identificador, datosExtraidos, err := segurasenior.LeerDatosDesdeJSON(nombreArchivo, nombrePoblacion)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedIdentificador, identificador)
+		assert.Equal(t, expectedDatos, datosExtraidos)
+	})
+	t.Run("Archivo No Encontrado", func(t *testing.T) {
+        nombreArchivo := "../testdata/no_existe.json"
+        nombrePoblacion := "Orcera"
+
+        _, _, err := segurasenior.LeerDatosDesdeJSON(nombreArchivo, nombrePoblacion)
+
+        assert.Error(t, err)
+        assert.Contains(t, err.Error(), "No existe el archivo")
+    })
+
 
 }
