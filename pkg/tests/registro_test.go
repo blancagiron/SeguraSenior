eb8362132b1a61511e69df9eda3dfb275fe71b7e
blancagiron <blancagiron@correo.ugr.es>
2025-01-18 21:51:39 +0100

    Features #44: - Se mejora legibilidad y se usa el constructor para crear DatosPoblacion para los registros - Se añade test que testa el comportamiento de la función al agregar un registro nuevo, si funciona, verifica que se puede crear un nuevo registro demografico sin errores y que el registro se agrega correctamente al mapa - Se añade test que testa el comportamiento de la función al intentar agregar registros duplicados para la misma población

--- pkg/tests/registro_test.go
+++ pkg/tests/registro_test.go
@@ -1,9 +1,10 @@
 package segurasenior
 
 import (
-	"testing"
 	"SeguraSenior/pkg/segurasenior"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestCrearRegistroDesdeDatos(t *testing.T) {
@@ -17,27 +18,15 @@ func TestCrearRegistroDesdeDatos(t *testing.T) {
 			},
 		}
 
-		datos := segurasenior.DatosPoblacion{
-			PoblacionTotal:          1000,
-			Hombres:                 500,
-			Mujeres:                 500,
-			EdadMedia:               30.5,
-			PorcentajeMenora20:      25.0,
-			PorcentajeMayora65:      15.0,
-			Nacimientos:             50,
-			Defunciones:             20,
-			TasaNatalidadSobre1000:  50.0,
-			TasaMortalidadSobre1000: 20.0,
-		}
-		
+		datos, err := segurasenior.NewDatosPoblacion(1000,500,500,30.5,25.0,15.0,50,20,50.0,20.0)
+		assert.NoError(t, err)
 
-		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, datos)
-
+		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, *datos)
 
 		assert.NoError(t, err)
 		assert.Equal(t, segurasenior.Creciente, registro.EstadoDeLaPoblacion)
 		assert.Contains(t, registro.EstadisticasPoblacion, identificador)
-		assert.Equal(t, datos, registro.EstadisticasPoblacion[identificador])
+		assert.Equal(t, *datos, registro.EstadisticasPoblacion[identificador])
 	})
 
 	t.Run("Crear registro con población decreciente", func(t *testing.T) {
@@ -50,24 +39,75 @@ func TestCrearRegistroDesdeDatos(t *testing.T) {
 			},
 		}
 
-		datos := segurasenior.DatosPoblacion{
-			PoblacionTotal:          1000,
-			Hombres:                 500,
-			Mujeres:                 500,
-			EdadMedia:               45.2,
-			PorcentajeMenora20:      15.0,
-			PorcentajeMayora65:      30.0,
-			Nacimientos:             10,
-			Defunciones:             50,
-			TasaNatalidadSobre1000:  10.0,
-			TasaMortalidadSobre1000: 50.0,
-		}
+		datos, err := segurasenior.NewDatosPoblacion(1000,500,500,45.2,15.0,30.0,10,50,10.0,50.0)
+		assert.NoError(t, err)
 
-		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, datos)
+		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, *datos)
 
 		assert.NoError(t, err)
 		assert.Equal(t, segurasenior.Decreciente, registro.EstadoDeLaPoblacion)
 		assert.Contains(t, registro.EstadisticasPoblacion, identificador)
-		assert.Equal(t, datos, registro.EstadisticasPoblacion[identificador])
+		assert.Equal(t, *datos, registro.EstadisticasPoblacion[identificador])
 	})
+}
+
+func TestAgregarRegistro(t *testing.T) {
+	t.Run("Agregar un registro nuevo", func(t *testing.T) {
+		registro := segurasenior.RegistroDemografico{
+			EstadisticasPoblacion: make(map[segurasenior.IdentificadorDatos]segurasenior.DatosPoblacion),
+			EstadoDeLaPoblacion:   segurasenior.Creciente,
+		}
+
+		identificador := segurasenior.IdentificadorDatos{
+			NombrePoblacion: "Pueblo Ejemplo",
+			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
+				Dia:  1,
+				Mes:  1,
+				Anio: 2025,
+			},
+		}
+
+		datos, err := segurasenior.NewDatosPoblacion(1000, 500, 500, 30.0, 25.0, 15.0, 50, 20, 50.0, 20.0)
+		assert.NoError(t, err)
+
+		err = registro.AgregarRegistro(identificador, *datos)
+
+		assert.NoError(t, err)
+		assert.Len(t, registro.EstadisticasPoblacion, 1)
+		assert.Equal(t, segurasenior.Creciente, registro.EstadoDeLaPoblacion)
+	})
+
+	t.Run("Intentar agregar un registro para la misma fecha", func(t *testing.T) {
+
+		registro := segurasenior.RegistroDemografico{
+			EstadisticasPoblacion: make(map[segurasenior.IdentificadorDatos]segurasenior.DatosPoblacion),
+			EstadoDeLaPoblacion:   segurasenior.Creciente,
+		}
+
+		identificador := segurasenior.IdentificadorDatos{
+			NombrePoblacion: "Pueblo Ejemplo",
+			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
+				Dia:  1,
+				Mes:  1,
+				Anio: 2025,
+			},
+		}
+		datos, err := segurasenior.NewDatosPoblacion(1600, 700, 900, 30.0, 25.0, 15.0, 10, 40, 10.0, 40.0)
+		assert.NoError(t, err)
+
+		err = registro.AgregarRegistro(identificador, *datos)
+		assert.NoError(t, err)
+		assert.Len(t, registro.EstadisticasPoblacion, 1)
+
+		err = registro.AgregarRegistro(identificador, *datos)
+		assert.Error(t, err)
+
+		expectedError := fmt.Sprintf("ya existe un registro para '%s' en la fecha %d/%d/%d",
+			identificador.NombrePoblacion, identificador.FechaDeDatos.Dia,
+			identificador.FechaDeDatos.Mes, identificador.FechaDeDatos.Anio)
+		assert.Contains(t, err.Error(), expectedError)
+
+		assert.Len(t, registro.EstadisticasPoblacion, 1)
+	})
+
 }
