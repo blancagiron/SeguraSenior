45659c776fa4f4259b348908975ca83fbfa5a939
blancagiron <blancagiron@correo.ugr.es>
2025-02-04 21:57:21 +0100

    [#69]. Método para validar que no haya registros duplicados
    - En #69 se describe el problema que puede haber para el cliente si hay registros duplicados, se implementa una función que devuelve error si encuentra dos registro para la misma fecha

--- pkg/segurasenior/registrodemografico.go
+++ pkg/segurasenior/registrodemografico.go
@@ -1,7 +1,11 @@
 package segurasenior
 
+import (
+	"fmt"
+)
 
 type EstadoPoblacion string
+
 
 const (
 	Decreciente EstadoPoblacion = "decreciente"
@@ -27,4 +31,13 @@ func CrearRegistroDesdeDatos(identificador IdentificadorDatos, datos DatosPoblac
 		EstadisticasPoblacion: map[IdentificadorDatos]DatosPoblacion{identificador: datos},
 		EstadoDeLaPoblacion:   estado,
 	}, nil
+}
+
+func ValidarRegistroExiste(estadisticas map[IdentificadorDatos]DatosPoblacion, identificador IdentificadorDatos) error {
+	if _, existe := estadisticas[identificador]; existe {
+		return fmt.Errorf("ya existe un registro para '%s' en la fecha %d/%d/%d",
+			identificador.NombrePoblacion, identificador.FechaDeDatos.Dia,
+			identificador.FechaDeDatos.Mes, identificador.FechaDeDatos.Anio)
+	}
+	return nil
 }
