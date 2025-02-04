d821685931445a298a56c3aa0947d4beae3577b4
blancagiron <blancagiron@correo.ugr.es>
2025-02-04 22:33:23 +0100

    [#70]. Ver cómo se crean los registros
    - En #70 también se habla sobre cómo los registros se rellenan a partir de IdentificadorDatos y DatosPoblacion
    - Se añaden una prueba unitaria para probar que un registro se crea de forma correcta a partir de un IdentificadorDatos y DatosPoblacion
    - Se usan los datos de Orcera para no usar valores mágicos

--- pkg/tests/registro_test.go
+++ pkg/tests/registro_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestCrearRegistroDesdeDatos(test *testing.T) {
-	test.Run("Crear registro con población creciente", func(test *testing.T) {
+	test.Run("Crear registro con población decreciente", func(test *testing.T) {
 		identificador := segurasenior.IdentificadorDatos{
 			NombrePoblacion: "Pueblo Feliz",
 			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
@@ -16,47 +16,13 @@ func TestCrearRegistroDesdeDatos(test *testing.T) {
 			},
 		}
 
-		datos, err := segurasenior.NewDatosPoblacion(1000,500,500,30.5,25.0,15.0,50,20,50.0,20.0)
+		datos, err := segurasenior.NewDatosPoblacion( PoblacionTotalOrcera,HombresOrcera,MujeresOrcera,EdadMediaOrcera,Menor20Orcera,Mayor65Orcera,NacimientosOrcera,DefuncionesOrcera,TasaNatalidadSobre1000Orcera,TasaMortalidadSobre1000Orcera)
 		if err != nil {
 			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
 		}
 
 		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, *datos)
 
-		if err != nil {
-			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
-		}
-		if registro.EstadoDeLaPoblacion != segurasenior.Creciente {
-			test.Errorf("Se esperaba estado Creciente, pero se obtuvo: %v", registro.EstadoDeLaPoblacion)
-		}
-		if _, ok := registro.EstadisticasPoblacion[identificador]; !ok {
-			test.Errorf("Se esperaba que el identificador estuviera presente en el registro")
-		}
-		if registro.EstadisticasPoblacion[identificador] != *datos {
-			test.Errorf("Los datos de población no coinciden con los esperados")
-		}
-	})
-
-	test.Run("Crear registro con población decreciente", func(test *testing.T) {
-		identificador := segurasenior.IdentificadorDatos{
-			NombrePoblacion: "Pueblo Triste",
-			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
-				Dia:  1,
-				Mes:  1,
-				Anio: 2023,
-			},
-		}
-
-		datos, err := segurasenior.NewDatosPoblacion(1000,500,500,45.2,15.0,30.0,10,50,10.0,50.0)
-		if err != nil {
-			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
-		}
-
-		registro, err := segurasenior.CrearRegistroDesdeDatos(identificador, *datos)
-
-		if err != nil {
-			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
-		}
 		if registro.EstadoDeLaPoblacion != segurasenior.Decreciente {
 			test.Errorf("Se esperaba estado Decreciente, pero se obtuvo: %v", registro.EstadoDeLaPoblacion)
 		}
@@ -66,5 +32,11 @@ func TestCrearRegistroDesdeDatos(test *testing.T) {
 		if registro.EstadisticasPoblacion[identificador] != *datos {
 			test.Errorf("Los datos de población no coinciden con los esperados")
 		}
+
+		if err != nil {
+			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
+		}
 	})
+
+	
 }
