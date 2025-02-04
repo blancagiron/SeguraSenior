b5d097bca7abb19a14133ca4bc3dca04e1973bdc
blancagiron <blancagiron@correo.ugr.es>
2025-02-04 23:07:43 +0100

    [Closes #70]. Comprobar lo que ocurre si intentamos añadir un registro duplicado
    - En #70 se menciona que se requiere una forma en la que asegurar que cada población tiene un único registro por fecha
    - Se crea un registro (no se usan otras funciones porque deben ser unitarios y no podemos confiar en el comportamiento de las otras funciones)
    - Se añade y se comprueba cómo se hace
    - Se intenta añadir de nuevo
    - Se comprueba que da error
    - Se usan los datos de Orcera para no usar valores mágicos

--- pkg/tests/registro_test.go
+++ pkg/tests/registro_test.go
@@ -2,6 +2,7 @@ package segurasenior
 
 import (
 	"SeguraSenior/pkg/segurasenior"
+	"fmt"
 	"testing"
 )
 
@@ -16,7 +17,7 @@ func TestCrearRegistroDesdeDatos(test *testing.T) {
 			},
 		}
 
-		datos, err := segurasenior.NewDatosPoblacion( PoblacionTotalOrcera,HombresOrcera,MujeresOrcera,EdadMediaOrcera,Menor20Orcera,Mayor65Orcera,NacimientosOrcera,DefuncionesOrcera,TasaNatalidadSobre1000Orcera,TasaMortalidadSobre1000Orcera)
+		datos, err := segurasenior.NewDatosPoblacion(PoblacionTotalOrcera, HombresOrcera, MujeresOrcera, EdadMediaOrcera, Menor20Orcera, Mayor65Orcera, NacimientosOrcera, DefuncionesOrcera, TasaNatalidadSobre1000Orcera, TasaMortalidadSobre1000Orcera)
 		if err != nil {
 			test.Errorf("No se esperaba error, pero se obtuvo: %v", err)
 		}
@@ -38,5 +39,46 @@ func TestCrearRegistroDesdeDatos(test *testing.T) {
 		}
 	})
 
+}
+
+func TestAgregarRegistro(test *testing.T) {
+	test.Run("Intentar agregar un registro para la misma fecha", func(test *testing.T) {
+		registro := segurasenior.RegistroDemografico{
+			EstadisticasPoblacion: make(map[segurasenior.IdentificadorDatos]segurasenior.DatosPoblacion),
+			EstadoDeLaPoblacion:   segurasenior.Creciente,
+		}
+
+		identificador := segurasenior.IdentificadorDatos{
+			NombrePoblacion: "Pueblo Feliz",
+			FechaDeDatos: segurasenior.FechaObtencionDeDatos{
+				Dia:  1,
+				Mes:  1,
+				Anio: 2023,
+			},
+		}
+
+		datos, _ := segurasenior.NewDatosPoblacion(PoblacionTotalOrcera, HombresOrcera, MujeresOrcera, EdadMediaOrcera, Menor20Orcera, Mayor65Orcera, NacimientosOrcera, DefuncionesOrcera, TasaNatalidadSobre1000Orcera, TasaMortalidadSobre1000Orcera)
 	
+		err := registro.AgregarRegistro(identificador, *datos)
+		if err != nil {
+			test.Errorf("No se esperaba error al agregar el registro, pero se obtuvo: %v", err)
+		}
+
+		err = registro.AgregarRegistro(identificador, *datos)
+		if err == nil {
+			test.Errorf("Se esperaba un error al intentar agregar un registro duplicado, pero no se obtuvo")
+		}
+
+		expectedError := fmt.Sprintf("ya existe un registro para '%s' en la fecha %d/%d/%d",
+			identificador.NombrePoblacion, identificador.FechaDeDatos.Dia,
+			identificador.FechaDeDatos.Mes, identificador.FechaDeDatos.Anio)
+
+		if err.Error() != expectedError {
+			test.Errorf("El mensaje de error esperado era '%s', pero se obtuvo '%s'", expectedError, err.Error())
+		}
+
+		if len(registro.EstadisticasPoblacion) != 1 {
+			test.Errorf("Se esperaba 1 registro, pero se obtuvieron: %d", len(registro.EstadisticasPoblacion))
+		}
+	})
 }
